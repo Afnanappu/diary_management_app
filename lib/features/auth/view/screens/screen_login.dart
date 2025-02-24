c086a8b4bcd3da25854fbad73ac2e2329777f302
AFNAN P <afnanp860@gmail.com>
2025-02-24 17:17:28 +0530

    login screen design ok

--- lib/features/auth/view/screens/screen_login.dart
+++ lib/features/auth/view/screens/screen_login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:dairy_management_app/core/constants/app_colors.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,76 +15,150 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.local_dining, size: 80, color: Colors.green),
-              SizedBox(height: 20),
-              Text(
-                "Dairy Management",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 40),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  prefixIcon: Icon(Icons.email),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
+      backgroundColor: AppColors.background,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            // Top Clipper Design
+            ClipPath(
+              clipper: WaveClipperTwo(),
+              child: Container(
+                height: 250,
+                decoration: BoxDecoration(color: AppColors.primaryDark),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.local_dining, size: 80, color: Colors.white),
+                      SizedBox(height: 10),
+                      Text(
+                        "Dairy Management",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              SizedBox(height: 20),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  prefixIcon: Icon(Icons.lock),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
+            ),
+
+            // Login Form
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 200),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 10,
+                            spreadRadius: 2,
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          // Admin Toggle Button
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                "Admin",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black54,
+                                ),
+                              ),
+                              Switch(
+                                value: isAdmin,
+                                activeColor: AppColors.primaryDark,
+                                onChanged: (value) {
+                                  setState(() {
+                                    isAdmin = value;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          // Email Field
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: "Email",
+                              prefixIcon: Icon(
+                                Icons.email,
+                                color: AppColors.primaryDark,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          // Password Field
+                          TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: "Password",
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: AppColors.primaryDark,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          // Login Button
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: AppColors.primaryDark,
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 40,
+                                vertical: 12,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            child: Text(
+                              isAdmin ? 'Login as Admin' : "Login",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Forgot Password?",
+                              style: TextStyle(color: AppColors.primaryDark),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ChoiceChip(
-                    label: Text("Login as Admin"),
-                    selected: isAdmin,
-                    selectedColor: Colors.green,
-                    onSelected: (selected) {
-                      setState(() {
-                        isAdmin = selected;
-                      });
-                    },
-                  ),
-                  TextButton(onPressed: () {}, child: Text("Forgot Password?")),
-                ],
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-                child: Text("Login", style: TextStyle(fontSize: 18)),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(home: LoginScreen()));
 }
